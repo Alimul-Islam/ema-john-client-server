e068f7a0e409f87af8242bfb502f99b1810bcb68
Alimul-Islam <alimulislam835@gmail.com>
2021-05-24 23:52:32 +0600

    first commit

---
+++ ema-john-with-auth-master/src/firebaseConfig.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3Dolr3HdYxVOXUFtZSmqkYQSZ-F6jE3k",
+  authDomain: "ema-john-auth-58fe1.firebaseapp.com",
+  projectId: "ema-john-auth-58fe1",
+  storageBucket: "ema-john-auth-58fe1.appspot.com",
+  messagingSenderId: "883693061968",
+  appId: "1:883693061968:web:27fdbc9a56c273a2db4e55"
+};
+export default firebaseConfig;
